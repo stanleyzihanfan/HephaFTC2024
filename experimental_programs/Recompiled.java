6e8dfb7fe8a34dde38e79f7fb6295435c5d81bde
Zihan Fan <stanleyzihanfan@gmail.com>
2025-03-01 13:11:08 -0800

    Changed comments, added acceleration reading to Imu class

--- experimental_programs/Recompiled.java
+++ experimental_programs/Recompiled.java
@@ -21,48 +21,102 @@ public class Recompiled {
 }
 //classes
 
-//motor class with all motor information
+/**
+ * Motor class with all motor information
+ */
 class Motor{
     protected DcMotor motor_=null;
     protected int velocity=0;
     protected double targetPosition=0;
+    /**
+     * Set motor
+     * @param motor motor as DcMotor object
+     */
+    protected void setMotor(DcMotor motor){
+        this.motor_=motor;
+    }
+    /**
+     * Set motor target position
+     * @param target double target position in encoder ticks
+     */
     protected void setTargetPos(double target){
         this.targetPosition=target;
         this.motor_.setTargetPosition(this.targetPosition);
     }
+    /**
+     * Set motor velocity
+     * @param velocity desired integer velocity for the motor in encoder ticks per second
+     */
     protected void setMotorVelocity(int velocity){
         this.velocity=velocity;
         this.motor_.setVelocity(this.velocity);
     }
 }
+/**
+ * IMU class
+ */
 class Imu{
-    protected IMU imu=null;
+    protected IMU imu_=null;
+    protected double secondsToUpdate=0.5;
+    /**
+     * Set IMU
+     * @param imu IMU object
+     */
     protected void setIMU(IMU imu){
-        this.imu=imu;
+        this.imu_=imu;
     }
+    /**
+     * Reset IMU yaw angle
+     */
     protected void resetYaw(){
-        this.imu.resetYaw();
+        this.imu_.resetYaw();
     }
+    /**
+     * Get robot yaw angle
+     * @return Yaw angle as a double in degrees
+     */
     protected double getHeading(){
-        YawPitchRollAngles orientation=this.imu.getRobotYawPitchRollAngles();
+        YawPitchRollAngles orientation=this.imu_.getRobotYawPitchRollAngles();
         return orientation.getYaw(AngleUnit.DEGREES);
     }
+    /**
+     * Get robot x and y acceleration
+     * @return Length 2 double arraw with acceleration as (x,y)
+     */
+    protected double[] getAcceleration(){
+        double[] acceleration=new double[2];
+        acceleration[0]=this.imu_.getAcceleration().xAccel;
+        acceleration[1]=this.imu_.getAcceleration().yAccel;
+        return acceleration;
+    }
 }
+/**
+ * Drivetrain class
+ */
 //drivetrain
 class Drivetrain{
-    //motor information lists
-    protected List<Motor> drivetrain=new ArrayList<Motor>();
-    protected IMU imu=null;
-    //set drivetrain motors, 0 is left front, 1 is left rear, 2 is right rear, 3 is right front
-    protected void setDrivetrain(List<DcMotor> motors){
+    //motor information list
+    protected Motor[] drivetrain=new Motor[4];
+    protected Imu imu=null;
+    /**
+     * Set drivetrain motors, 0 is left front, 1 is left rear, 2 is right rear, 3 is right front
+     * @param motors Array of DcMotor objects
+     */
+    protected void setDrivetrain(DcMotor[] motors){
         for (int i=0;i<4;i++){
             Motor motor=new Motor();
-            motor.motor_=motors.get(i);
-            drivetrain.add(motor);
+            motor.setMotor(motors[i]);
+            drivetrain[i]=motor;
         }
     }
 }
-//main robot class
+/**
+ * Main robot class
+ */
 class Robot{
-
+    //robot location information, relative to left-bottom corner of field(0,0).
+    //0 degrees theta is facing positive y direction.
+    protected double xPos=0;
+    protected double yPos=0;
+    protected double theta=0;
 }
