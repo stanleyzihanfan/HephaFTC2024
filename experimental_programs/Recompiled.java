7d3221ca49fa16078b287cc6491cd604b56b55f6
Zihan Fan <stanleyzihanfan@gmail.com>
2025-03-01 12:36:44 -0800

    Changed recompiling strategy-now useing multiple classes

--- experimental_programs/Recompiled.java
+++ experimental_programs/Recompiled.java
@@ -1,132 +1,68 @@
-
-import java.awt.dnd.DragSourceMotionListener;
-import javax.swing.plaf.InputMapUIResource;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import java.lang.Math;
+import java.lang.Thread;
+import java.util.*;
 
 public class Recompiled {
     
 }
-class drive{
-    //absolute robot values
-    private double RFront_encoder=0;
-    private double RFront_target=0;
-    private double LFront_encoder=0;
-    private double LFront_target=0;
-    private double RRear_encoder=0;
-    private double RRear_target=0;
-    private double LRear_encoder=0;
-    private double LRear_target=0;
-    private double wrist_horizontalPosition=0.35;
-    private double wrist_verticalPosition=1;
-    //claw position(0.3=closed,0.55=open)
-    private double claw_position=0.3;
-    private double arm_position=0;
-    private double arm_target=0;
-    //relative robot values(not nessesarily accurate, requires initiation value to be set at start)
-    //rotation in degrees relative to field(0=forward,(0,+180)=to the left,180=backwards,(0,-180)=to the right)
-    private double rotation_relativeField=0;
-    //x and y position relative to field, in cm. (0,0) is bottum left.
-    private double x_position_relativeField=0;
-    private double y_position_relativeField=0;
-    //robot objects
-    //arm servo+motor declaration
-    public DcMotor  armMotor       = hardwareMap.get(DcMotor.class,"arm_lift"); //the arm motor
-    public Servo    claw           = hardwareMap.get(DcMotor.class,"servo_claw"); //the claw servo
-    public Servo    wrist_vertical = hardwareMap.get(DcMotor.class,"servo_vertical"); //the wrist_vertical servo
-    public Servo    wrist_horizontal = hardwareMap.get(DcMotor.class,"servo_horizontal"); //the wrist_horizontal servo
-    // drivetrain wheel motor declaration
-    private DcMotor LFront=hardwareMap.get(DcMotor.class,"wheel_0");
-    private DcMotor LRear=hardwareMap.get(DcMotor.class,"wheel_1");
-    private DcMotor RFront=hardwareMap.get(DcMotor.class,"wheel_3");
-    private DcMotor RRear=hardwareMap.get(DcMotor.class,"wheel_2");
-    //linear slide motors
-    private DcMotor linearL=hardwareMap.get(DcMotor.class,"linearL");
-    private DcMotor linearR=hardwareMap.get(DcMotor.class,"linearR");
-    //set the orientation of the REV hub on the robot(the IMU location is on one of the corners of the robot)
-    RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
-    RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
-    RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
-    //IMU declatration
-    private IMU imu=hardwareMap.get(IMU.class,"imu");
-    //initiate runtime
-    private ElapsedTime runtime=new ElapsedTime();
+//classes
 
-    //Main getter function
-    public double[] get_full(){
-        double[] temp={RFront_encoder,LFront_encoder,RRear_encoder,LRear_encoder,wrist_horizontalPosition,wrist_verticalPosition,claw_position,arm_position,rotation_relativeField,x_position_relativeField,y_position_relativeField};
-        return temp;
+//motor class with all motor information
+class Motor{
+    protected DcMotor motor_=null;
+    protected int velocity=0;
+    protected double targetPosition=0;
+    protected void setTargetPos(double target){
+        this.targetPosition=target;
+        this.motor_.setTargetPosition(this.targetPosition);
     }
-
-    //absolute robot values getter function
-    public double[] get_absolute_robot_values(){
-        double[] temp={RFront_encoder,LFront_encoder,RRear_encoder,LRear_encoder,wrist_horizontalPosition,wrist_verticalPosition};
-        return temp;
+    protected void setMotorVelocity(int velocity){
+        this.velocity=velocity;
+        this.motor_.setVelocity(this.velocity);
     }
-
-    //relative robot values getter function
-    public double[] get_relative_robot_values(){
-        double[] temp={rotation_relativeField,x_position_relativeField,y_position_relativeField};
-        return temp;
+}
+class Imu{
+    protected IMU imu=null;
+    protected void setIMU(IMU imu){
+        this.imu=imu;
     }
-
-    //setter function to update absolute robot values(using robot readings)
-    public void update_read_robot(){
-        
+    protected void resetYaw(){
+        this.imu.resetYaw();
     }
-
-    /**
-     * get gyro heading in degrees
-     */
-    public double getHeading(){
-        YawPitchRollAngles orientation=imu.getRobotYawPitchRollAngles();
+    protected double getHeading(){
+        YawPitchRollAngles orientation=this.imu.getRobotYawPitchRollAngles();
         return orientation.getYaw(AngleUnit.DEGREES);
     }
-
-    /**
-     * Rotates the robot about itself a specified degrees.
-     * @param twist Degrees to rotate(negative->clockwise,positive->counterclockwise)
-     * @param speed
-     */
-    public void rotate(double twist,double speed){
-        double targetdirection=getHeading()+twist;
-        if (Math.abs(targetdirection)>180){
-            if (targetdirection<0){
-                targetdirection=360+targetdirection;
-            }
-            else{
-                targetdirection=360-targetdirection;
-            }
+}
+//drivetrain
+class Drivetrain{
+    //motor information lists
+    protected List<Motor> drivetrain=new ArrayList<Motor>();
+    protected IMU imu=null;
+    //set drivetrain motors, 0 is left front, 1 is left rear, 2 is right rear, 3 is right front
+    protected void setDrivetrain(List<DcMotor> motors){
+        for (int i=0;i<4;i++){
+            Motor motor=new Motor();
+            motor.motor_=motors.get(i);
+            drivetrain.add(motor);
         }
-        boolean target=(getHeading()>targetdirection);
-        while ((getHeading()>targetdirection)==target){
-            if (target){
-                LFront.setPower(speed);
-                LRear.setPower(speed);
-                RFront.setPower(-speed);
-                RRear.setPower(-speed);
-                LFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                LRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                RFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                RRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            }
-            else{
-                LFront.setPower(-speed);
-                LRear.setPower(-speed);
-                RFront.setPower(speed);
-                RRear.setPower(speed);
-                LFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                LRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                RFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                RRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            }
-        }
-        LFront.setTargetPosition(LFront.getCurrentPosition());
-        LRear.setTargetPosition(LRear.getCurrentPosition());
-        RFront.setTargetPosition(RFront.getCurrentPosition());
-        RRear.setTargetPosition(RRear.getCurrentPosition());
-        //stop motors
-        LFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        LRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        RFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        RRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
+}
+//main robot class
+class Robot{
+
 }
