ef23545b0b7dda50e1eaf463ec26af1e79e1e6fd
Zihan Fan <stanleyzihanfan@gmail.com>
2025-03-02 21:34:24 -0800

    Deleted unesessary parts due to unable to read IMU acceleration data, starting on encoder system to compensate

--- experimental_programs/Recompiled.java
+++ experimental_programs/Recompiled.java
@@ -1,3 +1,5 @@
+package org.firstinspires.ftc.teamcode.TestingFiles;
+
 import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
@@ -16,8 +18,11 @@ import java.lang.Math;
 import java.lang.Thread;
 import java.util.*;
 
+@Autonomous
+
 public class Recompiled {
-    
+
+	// todo: write your code here
 }
 //classes
 
@@ -25,21 +30,21 @@ public class Recompiled {
  * Motor class with all motor information
  */
 class Motor{
-    protected DcMotor motor_=null;
+    protected DcMotorEx motor_=null;
     protected int velocity=0;
-    protected double targetPosition=0;
+    protected int targetPosition=0;
     /**
      * Set motor
-     * @param motor motor as DcMotor object
+     * @param motor motor as DcMotorEx object
      */
-    protected void setMotor(DcMotor motor){
+    protected void setMotor(DcMotorEx motor){
         this.motor_=motor;
     }
     /**
      * Set motor target position
      * @param target double target position in encoder ticks
      */
-    protected void setTargetPos(double target){
+    protected void setTargetPos(int target){
         this.targetPosition=target;
         this.motor_.setTargetPosition(this.targetPosition);
     }
@@ -78,16 +83,6 @@ class Imu{
         YawPitchRollAngles orientation=this.imu_.getRobotYawPitchRollAngles();
         return orientation.getYaw(AngleUnit.DEGREES);
     }
-    /**
-     * Get robot x and y acceleration
-     * @return Length 2 double list with acceleration as [x,y]
-     */
-    protected double[] getAcceleration(){
-        double[] acceleration=new double[2];
-        acceleration[0]=this.imu_.getAcceleration().xAccel;
-        acceleration[1]=this.imu_.getAcceleration().yAccel;
-        return acceleration;
-    }
 }
 /**
  * Drivetrain class
@@ -96,12 +91,22 @@ class Imu{
 class Drivetrain{
     //motor information list
     protected Motor[] drivetrain=new Motor[4];
-    protected Imu imu=null;
+    /**
+     * Get motor encoder values
+     * @return Array of encoder values for each motor, [LF, LR, RR, RF]
+     */
+    protected int[] readMotorEncoders(){
+        int[] encoder=new int[4];
+        for (int i=0;i<4;i++){
+            encoder[i]=drivetrain[i].motor_.getCurrentPosition();
+        }
+        return encoder;
+    }
     /**
      * Set drivetrain motors, 0 is left front, 1 is left rear, 2 is right rear, 3 is right front
-     * @param motors Array of DcMotor objects
+     * @param motors Array of DcMotorEx objects
      */
-    protected void setDrivetrain(DcMotor[] motors){
+    protected void setDrivetrain(DcMotorEx[] motors){
         for (int i=0;i<4;i++){
             Motor motor=new Motor();
             motor.setMotor(motors[i]);
@@ -118,8 +123,6 @@ class Robot_Drivetrain{
     protected double xPos=0;
     protected double yPos=0;
     protected double theta=0;
-    protected double xVel=0;
-    protected double yVel=0;
     //seconds to update robot location via IMU
     protected double secondsToUpdate=0.5;
     //Seconds to wait for verifying robot location data via external sensors, TO BE IMPLEMENTED
@@ -128,13 +131,15 @@ class Robot_Drivetrain{
     //imu and drivetrain
     protected Imu imu=new Imu();
     protected Drivetrain drivetrain=new Drivetrain();
+    protected Drivetrain drivetrain_save=drivetrain;
     /**
      * Initialize drivetrain and IMU objects, change IMU orientation here
-     * @param motors Array of drivetrain DcMotor objects(0 is left front, 1 is left rear, 2 is right rear, 3 is right front)
+     * @param motors Array of drivetrain DcMotorEx objects(0 is left front, 1 is left rear, 2 is right rear, 3 is right front)
      * @param imu IMU object
      */
-    protected void initiateRobotParts(DcMotor[] motors, IMU imu){
+    protected void initiateRobotParts(DcMotorEx[] motors, IMU imu){
         this.drivetrain.setDrivetrain(motors);
+        this.drivetrain_save=this.drivetrain;
         this.imu.setIMU(imu);
         //set the orientation of the REV hub on the robot(the IMU location is on one of the corners of the robot)
         RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
@@ -153,39 +158,32 @@ class Robot_Drivetrain{
         this.theta=theta;
     }
     /**
-     * Update robot x&y velocity values using IMU, also updates theta
-     * @return Updated velocities as [x velocity, y velocity]
-     */
-    protected double[] updateVelocity(){
-        this.theta=this.imu.getHeading();
-        double[] acceleration=this.imu.getAcceleration();
-        //robot-centric x velocity
-        double RCxVel=acceleration[0]*this.secondsToUpdate;
-        //robot-centric y velocity
-        double RCyVel=acceleration[1]*this.secondsToUpdate;
-        //calculate degrees of rotation based on theta(in rotation matrix, theta is angle of rotation in counterclockwise direction, in degrees)
-        double rotationTheta=0;
-        if (this.theta<0){
-            rotationTheta=180+Math.abs(this.theta);
-        }
-        else{
-            rotationTheta=this.theta;
-        }
-        rotationTheta=360-rotationTheta;
-        //rotation matrix conversion to field-centric
-        double tempxVel=RCxVel*Math.cos(Math.toRadians(rotationTheta))-RCyVel*Math.sin(Math.toRadians(rotationTheta));
-        double tempyVel=RCxVel*Math.sin(Math.toRadians(rotationTheta))+RCyVel*Math.cos(Math.toRadians(rotationTheta));
-        this.xVel=tempxVel;
-        this.yVel=tempyVel;
-        return new double[]{this.xVel, this.yVel};
-    }
-    /**
      * Update robot position using IMU
      * @return Updated coordinates in an array [xPos, yPos, theta]
      */
     protected double[] updatePos(){
-        double[] coordChange=this.imu.getAcceleration();
-        //use rotation matrix to rotate acceleration to field-centric coordinates
+        //update theta
+        this.theta=this.imu.getHeading();
+        //read motor encoder changes
+        int[] past_encoders=new int[4];
+        int[] current_encoders=new int[4];
+        double[] decomposed_motor_vectors=new double[4];
+        double[] robot_vectors = {0, 0};
+        past_encoders=this.drivetrain_save.readMotorEncoders();
+        current_encoders=this.drivetrain.readMotorEncoders();
+        for (int i=0;i<4;i++){
+            decomposed_motor_vectors[i]=Math.sqrt(Math.pow(current_encoders[i]-past_encoders[i],2)/2);
+            if (current_encoders[i]-past_encoders[i]<0){
+                decomposed_motor_vectors[i]*=-1;
+            }
+        }
+        //calculate robot vectors
+        //positive x and y facing vectors(LF and RR)
+        robot_vectors[0]=robot_vectors[0]+decomposed_motor_vectors[0]+decomposed_motor_vectors[2];
+        robot_vectors[1]=robot_vectors[1]+decomposed_motor_vectors[0]+decomposed_motor_vectors[2];
+        //negative x and positive y facing vectors(LR and RF)
+        robot_vectors[0]=robot_vectors[0]-decomposed_motor_vectors[1]-decomposed_motor_vectors[3];
+        robot_vectors[1]=robot_vectors[1]+decomposed_motor_vectors[1]+decomposed_motor_vectors[3];
         return new double[]{this.xPos,this.yPos,this.theta};
     }
 }
